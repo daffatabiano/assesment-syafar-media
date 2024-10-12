de97981cc6b4b1dea4d490f18e47758770daecdb
unknown <daffatabianoo@gmail.com>
2024-10-12 11:14:49 +0700

    link

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-    <div className="text-3xl text-blue-500">
+    <div className="text-3xl flex flex-col justify-center items-center gap-8 text-blue-500">
       <h1>Home</h1>
       <p>untuk username: admin</p>
       <p>untuk password: admin123</p>
       <Link
-        className="bg-slate-400 p-4 mt-12 text-slate-50 hover:bg-slate-500 hover:text-slate-50"
+        className="bg-slate-400 p-4 text-slate-50 hover:bg-slate-500 hover:text-slate-50"
         to="/dashboard">
         go to dashboard
       </Link>
