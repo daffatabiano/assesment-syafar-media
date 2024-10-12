80de6c0e9b9aa2ec5b9828c477fff666c87ee7fc
unknown <daffatabianoo@gmail.com>
2024-10-12 11:10:58 +0700

    link

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+
 export default function Home() {
   return (
     <div className="text-3xl text-blue-500">
       <h1>Home</h1>
+      <p>untuk username: admin</p>
+      <p>untuk password: admin123</p>
+      <Link to="/dashboard">dashboard</Link>
     </div>
   );
 }
